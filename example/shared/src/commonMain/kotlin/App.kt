d452e7c444c030af826c393d5c6617db37ef3864
Александр Жиркевич <sasha.zhirkevich@gmail.com>
2024-05-09 21:52:11 +0300

    cleanup

--- example/shared/src/commonMain/kotlin/App.kt
+++ example/shared/src/commonMain/kotlin/App.kt
@@ -1,56 +1,27 @@
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.drawText
-import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import io.github.alexzhirkevich.klyrics.Lyrics
 import klyrics.example.shared.generated.resources.Res
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 
-@OptIn(ExperimentalResourceApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalResourceApi::class)
 
 
 @Composable
 fun App() {
-
-//    val style = androidx.compose.ui.text.TextStyle(
-//        fontSize = 32.sp
-//    )
-//
-//    val measurer = rememberTextMeasurer()
-//
-//    val brush =  SolidColor(Color.White.copy(.5f))
-//    Spacer(Modifier.fillMaxSize().background(Color.Black).drawWithCache {
-//        onDrawBehind {
-//            val t1 = measurer.measure("White with 0.5 alpha", style)
-//
-//            drawText(t1, brush = brush)
-//        }
-//    })
-//    return
 
     MaterialTheme(
         colorScheme = darkColorScheme()
